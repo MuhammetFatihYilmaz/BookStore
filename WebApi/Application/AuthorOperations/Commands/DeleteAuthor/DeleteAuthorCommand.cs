6f521519c3c130114996224c66ea8ea8c983448c
MuhammetFatihYilmaz <muhammet.yilmaz.1995@gmail.com>
2022-02-15 13:05:19 +0300

    Added Delete Author Commands

---
+++ WebApi/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+
+namespace WebApi.Application.AuthorOperations.Commands.DeleteAuthor
+{
+    public class DeleteAuthorCommand
+    {
+        private readonly BookStoreDbContext _context;
+        public int AuthorId { get; set; }
+        public DeleteAuthorCommand(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x=> x.Id ==AuthorId);
+            if(author is null)
+                throw new InvalidOperationException("Silinecek yazar bulunamadı.");
+
+            _context.Authors.Remove(author);
+            _context.SaveChanges();
+        }
+    }
+}
