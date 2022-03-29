e84d1436d1831bef95f53608617d9bdef2b52299
MuhammetFatihYilmaz <muhammet.yilmaz.1995@gmail.com>
2022-03-29 20:09:33 +0300

    Added CreateAuthorCommandTests

---
+++ Tests/WebApi.UnitTests/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommandTests.cs
@@ -0,0 +1,41 @@
+using System;
+using AutoMapper;
+using FluentAssertions;
+using Tests.TestSetup;
+using WebApi;
+using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using WebApi.Application.GenreOperations.Commands.CreateGenre;
+using WebApi.Entities;
+using Xunit;
+
+namespace Tests.Application.AuthorOperations.Commands.CreateAuthor
+{
+    public class CreateAuthorCommandTests: IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public CreateAuthorCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenAlreadyExistAuthorNameGiven_InvalidOperationExeption_ShouldBeReturn()
+        {
+            //Arrange
+            var author = new Author(){Name="WhenAlreadyExistAuthorNameGiven_InvalidOperationExeption_ShouldBeReturn", Surname = "Test Surname", BirthDate = DateTime.Now.Date.AddYears(-80) };
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+            
+            CreateAuthorCommand command = new CreateAuthorCommand(_context,_mapper);
+            command.Model = new CreateAuthorModel(){Name = author.Name, Surname = author.Surname, BirthDate = DateTime.Now.Date.AddYears(-10)};
+            //Act & Assert
+            FluentActions
+                .Invoking(()=> command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Yazar zaten mevcut");
+        }
+
+    }
+
+}
